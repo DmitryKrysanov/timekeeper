edabf45ca33712bdf379363b85b2c3c8978d590e
Dima <dim.kry.lis@gmail.com>
2020-12-24 13:18:15 +0200

    Primary button added

---
+++ src/components/UI/PrimaryButton/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Loader from '../Loader';
+
+interface IPrimaryButton {
+  isLoad: boolean;
+  children: any;
+  color: 'inherit' | 'primary' | 'secondary' | 'default' | undefined;
+  variant: 'text' | 'outlined' | 'contained' | undefined;
+  onClick: () => void;
+}
+
+export default function PrimaryButton({
+  isLoad,
+  children,
+  ...restProps
+}: IPrimaryButton): JSX.Element {
+  return (
+    <Button {...restProps}>
+      {isLoad ? <Loader color="inherit" size={24} /> : children}
+    </Button>
+  );
+}
